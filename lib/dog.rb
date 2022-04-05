5707e05bca457fc368fa2800ef49d01d484382cf
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-04-05 17:28:56 -0400

    All tests passing

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+  # SETTER
+  def name=(name)
+    @name = name
+  end
 
+  # GETTER
+  def name
+    @name
+  end
+
+  # SETTER
+  def breed=(breed)
+    @breed = breed
+  end
+
+  # GETTER
+  def breed
+    @breed
+  end
 end
